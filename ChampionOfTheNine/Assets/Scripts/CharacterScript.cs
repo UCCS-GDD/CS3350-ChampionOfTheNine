da1237425105b80f27ca3e2acc670555a066a5cc
awesomeunleashed <ashmanisnow@gmail.com>
2015-09-23 11:01:39 -0600

    Added basic player without movement

---
+++ ChampionOfTheNine/Assets/Scripts/CharacterScript.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// Abstract parent script that controls characters
+/// </summary>
+[RequireComponent(typeof(AudioSource))]
+public abstract class CharacterScript : MonoBehaviour
+{
+    #region Fields
+
+    [SerializeField]protected float maxHealth;
+    protected float health;
+    protected AudioSource audioSource;
+
+    public Image healthBar;
+
+    #endregion
+
+    #region Properties
+
+    /// <summary>
+    /// Gets and sets the character's health, setting the health bar appropriately
+    /// </summary>
+    private float Health
+    {
+        get { return health; }
+        set
+        {
+            health = value;
+            healthBar.fillAmount = health / maxHealth;
+        }
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    /// <summary>
+    /// Damages the character by the given amount
+    /// </summary>
+    /// <param name="amount">the amount</param>
+    public virtual void Damage(float amount)
+    {
+        // Subtracts from the health
+        Health -= amount;
+    }
+
+    #endregion
+
+    #region Protected Methods
+
+    /// <summary>
+    /// Start is called once on object creation
+    /// </summary>
+    protected virtual void Start()
+    {
+        Health = maxHealth;
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    #endregion
+}
---
+++ ChampionOfTheNine/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// Script that controls the player
+/// </summary>
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(AudioListener))]
+public class PlayerScript : CharacterScript
+{
+    #region Fields
+
+
+    #endregion
+
+    #region Properties
+
+
+
+    #endregion
+
+    #region Protected Methods
+
+    ///// <summary>
+    ///// Start is called once on object creation
+    ///// </summary>
+    //protected override void Start()
+    //{
+    //    base.Start();
+    //}
+
+    #endregion
+
+    #region Private Methods
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    private void Update()
+    {
+
+    }
+
+    #endregion
+}
