50d41e3a533e3be85ae2a5218f572e6239cdbe44
awesomeunleashed <ashmanisnow@gmail.com>
2015-09-30 22:01:41 -0600

    Updated cooldown/duration UI, added enemy spawner, updated constants

---
+++ ChampionOfTheNine/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// Script that controls the enemy spawner
+/// </summary>
+public class EnemySpawner : MonoBehaviour
+{
+    #region Fields
+
+    [SerializeField]GameObject enemyRanger;
+
+    #endregion
+
+    #region Private Methods
+
+    /// <summary>
+    /// Start is called once on object creation
+    /// </summary>
+    private void Start()
+    {
+        InvokeRepeating("SpawnEnemy", 1, 5);
+    }
+
+    /// <summary>
+    /// Spawns an enemy
+    /// </summary>
+    private void SpawnEnemy()
+    {
+        Instantiate(enemyRanger, transform.position, transform.rotation);
+    }
+
+    #endregion
+}
