0377aed1eac64236442fcad114d5f07e1a2b0fa6
darrare <ryandarras@gmail.com>
2015-11-11 17:35:58 -0700

    lol

---
+++ ChampionOfTheNine/Assets/Scripts/BloodParticle.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BloodParticle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Invoke ("DeathClock", 2);
+	}
+
+	void DeathClock()
+	{
+		Destroy (gameObject);
+	}
+}
